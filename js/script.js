49f4d856da7a325bfdf926237719bd0d0441701f
Vargadio <anggagumilang3622@gmail.com>
2024-10-29 12:55:23 +0700

    first init github

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* typing animation */
+var typed = new Typed(".typing", {
+  strings: [
+    "",
+    "Web Developer",
+    "Web Designer",
+    "Fornt End Developer",
+    "Back End Developer",
+  ],
+  typedSpeed: 100,
+  backSpeed: 60,
+  loop: true,
+});
